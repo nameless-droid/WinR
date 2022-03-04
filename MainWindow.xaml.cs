71a9d0341defc9bbf19ae5991c1955ee99f9ad5d
BuildTools <unconfigured@null.spigotmc.org>
2022-03-04 11:47:24 +0100

    changed starting position to be in the bottom right corner, added close button (material ui), re-enabled material ui for xaml, window activates and shows itself when the run prompt appears, added support for german Dokumente, Musik, ..., added -debug argument

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -17,6 +18,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Path = System.IO.Path;
+using Forms = System.Windows.Forms;
+using Drawing = System.Drawing;
 
 namespace WinR
 {
@@ -28,10 +31,43 @@ namespace WinR
         public MainWindow()
         {
             InitializeComponent();
+            //test.Visibility = Visibility.Hidden;
         }
+
+        private int taskbarHeight => Forms.Screen.PrimaryScreen.Bounds.Height - Forms.Screen.PrimaryScreen.WorkingArea.Height;
+        //private Drawing.Point getCurrentPos => new Drawing.Point((int)Left, (int)Top);
+        private Forms.Screen getCurrentScreen => Forms.Screen.FromPoint(new Drawing.Point((int)Left, (int)Top));
+        private int getCurrentScreenLeft => getCurrentScreen.Bounds.Left;
+        private int getCurrentScreenY => getCurrentScreen.Bounds.Y;
+        //private int currentScreenTaskbarHeight => Forms.Screen.FromPoint(getCurrentPos).Bounds.Height - Forms.Screen.PrimaryScreen.WorkingArea.Height;
+
+        //public Button test = new Button();
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            #region position
+
+            //var toolbarHeight = SystemParameters.PrimaryScreenHeight - SystemParameters.FullPrimaryScreenHeight - SystemParameters.WindowCaptionHeight;
+
+            //private int WindowsTaskBarHeight => Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.WorkingArea.Height;
+
+            this.Left = getCurrentScreenLeft + 10;
+            this.Top = getCurrentScreen.Bounds.Height - taskbarHeight - this.ActualHeight - 10;
+
+                #endregion
+
+            //DataSet myDataSet;
+            //myDataSet
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args != null)
+            {
+                if (args.Length > 1 && args[1] == "-debug")
+                {
+                    debugCard.Visibility = Visibility.Visible;
+                }
+            }
+
             //string systemPath = "C:";
             string systemPath = Path.GetPathRoot(Environment.SystemDirectory);
             //string systemPath = Path.GetPathRoot(Environment.dir);
@@ -47,13 +83,18 @@ namespace WinR
                 }
             }
 
+            foreach (string s in Directory.GetDirectories(systemPath))
+            {
+                names.Add("\\" + s);
+            }
 
-            names.Add("WPF rocks");
-            names.Add("WCF rocks");
-            names.Add("XAML is fun");
-            names.Add("WPF rules");
-            names.Add("WCF rules");
-            names.Add("WinForms not");
+
+            //names.Add("WPF rocks");
+            //names.Add("WCF rocks");
+            //names.Add("XAML is fun");
+            //names.Add("WPF rules");
+            //names.Add("WCF rules");
+            //names.Add("WinForms not");
 
 
             FilteredComboBox1.Items.Clear();
@@ -78,6 +119,7 @@ namespace WinR
             //string window = GetActiveWindowTitle();
             IntPtr intPtr = GetForegroundWindow();
             string windowClass = GetActiveWindowClass();
+            string windowTitle = GetActiveWindowTitle();
             //Console.WriteLine(window);
             while (true)
             {
@@ -89,7 +131,7 @@ namespace WinR
                 //}
 
 
-                if (windowClass == "#32770")
+                if (windowClass == "#32770" && (windowTitle.Equals("Ausführen") || windowTitle.Equals("Run")))
                 {
                     windowClass = GetActiveWindowClass();
                     //this.Show();
@@ -108,6 +150,7 @@ namespace WinR
                 {
                     windowClass = GetActiveWindowClass();
                     intPtr = GetForegroundWindow();
+                    windowTitle = GetActiveWindowTitle();
                     Console.WriteLine(windowClass);
                     Debug.WriteLine(windowClass);
                 }
@@ -233,23 +276,233 @@ namespace WinR
 
         private void FilteredComboBox1_KeyDown(object sender, KeyEventArgs e)
         {
+            string userDir = Path.Combine(Path.GetPathRoot(Environment.SystemDirectory), "Users", Environment.UserName);
+            string s = ((FilteredComboBox)sender).Text;
+            //if (string.IsNullOrEmpty(s))
             if (e.Key == Key.Enter)
             {
+                /*
+                float d = dd("Dokumente");
+                float d2 = dd("Musik");
+                float d3 = dd2("Musik");
+                float d4 = dd33("Musik");
+                float d5 = dd33("3d-Objekte");
+                float d6 = dd33("3D Objekte");
 
-                string s = ((FilteredComboBox)sender).Text;
-                s = s.Replace("/", "");
+                if (d > 0.7)
+                {
+                    ;
+                }
 
-                //Process.Start(((FilteredComboBox)sender).Text);
-                ProcessStartInfo psi = new ProcessStartInfo();
-                //psi.UseShellExecute = false;    
-                //psi.RedirectStandardOutput = true;
-                //psi.RedirectStandardError = true;
-                //psi.WorkingDirectory = Path.Combine(Path.GetPathRoot(Environment.SystemDirectory), "Users", Environment.UserName);
-                psi.FileName = "explorer";
-                psi.Arguments = s;
-                Process.Start(psi); 
+                if (dd2("Musik") > 0.7)
+                {
+                    ;
+                }
+                
+                if (dd33("Musik") > 0.7)
+                {
+                    ;
+                }
+
+                s = s.Replace("\\\\", "\\");
+                */
+
+
+
+                //s = s.Replace("/", "");
+                if (s.StartsWith("/") || s.StartsWith("\\"))
+                {
+                    s = s.Remove(0, 1);
+                    //Process.Start(((FilteredComboBox)sender).Text);
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    //psi.UseShellExecute = false;    
+                    //psi.RedirectStandardOutput = true;
+                    //psi.RedirectStandardError = true;
+                    //psi.WorkingDirectory = Path.Combine(Path.GetPathRoot(Environment.SystemDirectory), "Users", Environment.UserName);
+                    psi.FileName = "explorer";
+                    psi.Arguments = s;
+                    Process.Start(psi);
+                }
+                //else if (Directory.Exists(Path.Combine(userDir, s)))
+                //{
+                //    string path = Path.Combine(userDir, s);
+                //    Process.Start(path);
+                //}
+                else
+                {
+                    string pathToOpen = "";
+
+                    //if (dd33(s) > 0.6)
+                    string[] list = { "Pictures", "Desktop", "3D Objects", "Documents", "Music", "Downloads", "Videos" };
+                    foreach (var item in list)
+                    {
+                        if (dd2ee(s, item) > 0.6)
+                        {
+                            switch (item)
+                            {
+                                case "Pictures":
+                                    pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+                                    break;
+                                case "Desktop":
+                                    pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                                    break;
+                                case "3D Objects":
+                                    //pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.);
+                                    break;
+                                case "Documents":
+                                    pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                                    break;
+                                case "Music":
+                                    pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                                    break;
+                                case "Downloads":
+                                    //pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.);
+                                    pathToOpen = "{374DE290-123F-4565-9164-39C4925E467B}";
+                                    break;
+                                case "Videos":
+                                    pathToOpen = Environment.GetFolderPath(Environment.SpecialFolder.MyVideos);
+                                    break;
+                                default:
+                                    break;
+                            }
+                            break;
+                        }
+                    }
+
+                    if (pathToOpen != "")
+                    {
+                        if (pathToOpen.StartsWith("{") && pathToOpen.EndsWith("}"))
+                        {
+                            //C:\Windows\explorer.exe shell:::{374DE290-123F-4565-9164-39C4925E467B}
+                            ProcessStartInfo psi1 = new ProcessStartInfo();
+                            psi1.CreateNoWindow = true;
+                            psi1.WorkingDirectory = Path.Combine(Path.GetPathRoot(Environment.SystemDirectory), "Users", Environment.UserName);
+                            psi1.FileName = "explorer.exe";
+                            psi1.Arguments = "shell:::" + pathToOpen;
+                            Process.Start(psi1);
+                            return;
+                        }
+                        else
+                        {
+                            ProcessStartInfo psi1 = new ProcessStartInfo();
+                            psi1.CreateNoWindow = true;
+                            psi1.WorkingDirectory = Path.Combine(Path.GetPathRoot(Environment.SystemDirectory), "Users", Environment.UserName);
+                            psi1.FileName = "explorer.exe";
+                            psi1.Arguments = pathToOpen;
+                            Process.Start(psi1);
+                            return;
+                        }
+
+                    }
+
+
+
+                    ProcessStartInfo psi = new ProcessStartInfo();
+                    psi.CreateNoWindow = true;
+                    psi.WorkingDirectory = Path.Combine(Path.GetPathRoot(Environment.SystemDirectory), "Users", Environment.UserName);
+                    psi.FileName = "cmd";
+                    psi.Arguments = "/c start " + s;
+
+
+                    //psi.FileName = "powershell";
+                    //psi.Arguments = "start " + s;
+                    //psi.UseShellExecute = true;
+                    //psi.ErrorDialog = true;
+                    //psi.RedirectStandardError = true;
+                    //psi.FileName = s;
+                    //psi.Arguments = s;
+
+                    Process.Start(psi);
+                }
+
+
+
 
             }
         }
+
+        private void CloseClick_HideWindow(object sender, RoutedEventArgs e)
+        {
+            //SetWindowPos(new WindowInteropHelper(this).Handle, HWND_NOTOPMOST, 0, 0, 0, 0, SWP.HIDEWINDOW | SWP.NOMOVE | SWP.NOSIZE);
+            App.Current.Shutdown();
+        }
+
+        public float dd(string s)
+        {
+            int result = 0;
+            string str = "Documents";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == str[i])
+                {
+                    result++;
+                }
+            }
+            return (float)result / str.Length;
+            //return 2.1f;
+        }
+
+        public float dd2(string s)
+        {
+            int result = 0;
+            string str = "Music";
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == str[i])
+                {
+                    result++;
+                }
+            }
+            return (float)result / str.Length;
+        }
+
+        public float dd33(string s)
+        {
+            //int result = 0;
+            List<float> results = new List<float>();
+            //string str = "Music";
+            string[] list = { "Pictures", "Desktop", "3D Objects", "Documents", "Music", "Downloads", "Videos" };
+
+            int tmp = 0;
+            for (int k = 0; k < list.Length; k++)
+            {
+                string current = list[k];
+                for (int i = 0; i < s.Length; i++)
+                {
+                    //if (list[k][i] == str[i])
+                    //char c = list[k][i];
+                    //if (s[i] == c)
+                    if (s.Length > i && current.Length > i && s[i] == current[i])
+                    {
+                        tmp++;
+                    }
+                }
+                //results.Add(tmp / list[k].Length);
+                results.Add((float)tmp / current.Length);
+                tmp = 0;
+            }
+
+           float max = results.Max();
+
+           return max;
+           //return (float)max / str.Length;
+        }
+
+        public float dd2ee(string s, string s2)
+        {
+            int result = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s.Length > i && s2.Length > i && s[i] == s2[i])
+                {
+                    result++;
+                }
+            }
+            return (float)result / s2.Length;
+        }
+
     }
 }
