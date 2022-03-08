36df3c7c091be30d2cfea350d479220e92798b60
Simon Staudt <simon.staudt1@gmail.com>
2022-03-08 18:55:50 +0100

    cant use xaml islands in .net6 with wpf

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Windows.UI.Xaml.Controls;
 
 namespace WinR
 {
@@ -24,5 +25,33 @@ namespace WinR
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AutoSuggestBoxHost_ChildChanged(object sender, EventArgs e)
+        {
+            if (autoSuggestBoxHost.Child is Windows.UI.Xaml.Controls.AutoSuggestBox autoSuggestBox)
+            {
+                //autoSuggestBox.TextChanged += AutoSuggestBox_TextChanged;
+                //autoSuggestBox.TextChanged += (s, args) =>
+                //{
+                //    MessageBox.Show("Hi from UWP Button!");
+
+                //};
+                //autoSuggestBox.add_TextChanged(AutoSuggestBox_TextChanged);
+                //autoSuggestBox.add_TextChanged((s, args) =>
+                //{
+
+                //});
+            }
+        }
+
+        //private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        //{
+            
+        //}
     }
 }
