a53ca98983ea643d6964e2d6d5c1271927a87449
Simon Staudt <simon.staudt1@gmail.com>
2022-03-08 20:04:38 +0100

    added Kinnara/ModernWpf, window now activates when win + r is pressed, added autosuggestbox (ModernWpf) and added some items to it

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ModernWpf.Controls;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,7 +14,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Windows.UI.Xaml.Controls;
+//using Windows.UI.Xaml.Controls;
+
+using Path = System.IO.Path;
 
 namespace WinR
 {
@@ -21,6 +25,8 @@ namespace WinR
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<string> list = new List<string>();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -28,30 +34,113 @@ namespace WinR
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
+            //autoSuggestBox.DataContext = this;
 
-        }
+            string systemPath = Path.GetPathRoot(Environment.SystemDirectory);
+            //string systemPath = Path.GetPathRoot(Environment.dir);
+            //FileInfo fileInfo = new FileInfo(systemPath);
+            string[] s1 = Environment.GetLogicalDrives();
+            List<string> names = new List<string>();
 
-        private void AutoSuggestBoxHost_ChildChanged(object sender, EventArgs e)
-        {
-            if (autoSuggestBoxHost.Child is Windows.UI.Xaml.Controls.AutoSuggestBox autoSuggestBox)
+            foreach (var item in s1)
             {
-                //autoSuggestBox.TextChanged += AutoSuggestBox_TextChanged;
-                //autoSuggestBox.TextChanged += (s, args) =>
-                //{
-                //    MessageBox.Show("Hi from UWP Button!");
-
-                //};
-                //autoSuggestBox.add_TextChanged(AutoSuggestBox_TextChanged);
-                //autoSuggestBox.add_TextChanged((s, args) =>
-                //{
-
-                //});
+                foreach (string s in Directory.GetFileSystemEntries(item))
+                {
+                    names.Add("/" + s);
+                }
             }
+
+            foreach (string s in Directory.GetDirectories(systemPath))
+            {
+                names.Add("\\" + s);
+            }
+
+            list.AddRange(names);
+
+            //names.Add("WPF rocks");
+            //names.Add("WCF rocks");
+            //names.Add("XAML is fun");
+            //names.Add("WPF rules");
+            //names.Add("WCF rules");
+            //names.Add("WinForms not");
+
+
+            autoSuggestBox.Items.Clear();
+
+
+            autoSuggestBox.IsTextSearchEnabled = false;
+            autoSuggestBox.ItemsSource = names;
+
+            //autoSuggestBox.ItemsSource = this;
+
+            WinKeyboardHook.Sub();
         }
+
+        private void AutoSuggestBox_TextChanged(ModernWpf.Controls.AutoSuggestBox sender, ModernWpf.Controls.AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void AutoSuggestBox_SuggestionChosen(ModernWpf.Controls.AutoSuggestBox sender, ModernWpf.Controls.AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+            ;
+        }
+
+        private void AutoSuggestBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            /*
+            AutoSuggestBox autoSuggestBox = sender as AutoSuggestBox;
+            //if (autoSuggestBox != null)
+            System.ComponentModel.ICollectionView collectionView = autoSuggestBox.ItemsSource as System.ComponentModel.ICollectionView;
+            //string d = collectionView.Cast<ModernWpf.Controls.AutoSuggestBox>().ToList();
+            IEnumerable<string> valueList = collectionView.Cast<string>();
+            //List<string> valueList = (autoSuggestBox.ItemsSource as System.ComponentModel.ICollectionView).Cast<string>().ToList();
+            */
+
+            //AutoSuggestBox autoSuggestBox = sender as AutoSuggestBox;
+            //List<string> valueList = autoSuggestBox.ItemsSource as List<string>;
+
+
+            if (e.Key == Key.Enter)
+            {
+                //if (((AutoSuggestBox)sender).ItemsSource.)
+                if (list.Contains(autoSuggestBox.Text))
+                {
+                    try
+                    {
+                        System.Diagnostics.Process.Start(autoSuggestBox.Text);
+                    }
+                    catch (Exception)
+                    {
+                        MessageBox.Show("Error");
+                    }
+
+                }
+            }
+
+        }
+
+        //private void AutoSuggestBoxHost_ChildChanged(object sender, EventArgs e)
+        //{
+        //    if (autoSuggestBoxHost.Child is Windows.UI.Xaml.Controls.AutoSuggestBox autoSuggestBox)
+        //    {
+        //        //autoSuggestBox.TextChanged += AutoSuggestBox_TextChanged;
+        //        //autoSuggestBox.TextChanged += (s, args) =>
+        //        //{
+        //        //    MessageBox.Show("Hi from UWP Button!");
+
+        //        //};
+        //        //autoSuggestBox.add_TextChanged(AutoSuggestBox_TextChanged);
+        //        //autoSuggestBox.add_TextChanged((s, args) =>
+        //        //{
+
+        //        //});
+        //    }
+        //}
 
         //private void AutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
         //{
-            
+
         //}
     }
 }
---
+++ WinKeyboardHook.cs
@@ -0,0 +1,130 @@
+﻿using Gma.UserActivityMonitor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinR
+{
+    internal class WinKeyboardHook
+    {
+        public static void Sub()
+        {
+            HookManager.KeyPress += HookManager_KeyPress;
+            HookManager.KeyDown += HookManager_KeyDown;
+            HookManager.KeyUp += HookManager_KeyUp;
+        }
+
+        public static List<Keys> keysDown = new List<Keys>();
+        private static void HookManager_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Used for overriding the Windows default hotkeys
+            if (keysDown.Contains(e.KeyCode) == false)
+            {
+                keysDown.Add(e.KeyCode);
+            }
+
+            /*
+            if (e.KeyCode == Keys.Right && WIN())
+            {
+                e.Handled = true;
+                //Do what you want when this key combination is pressed
+            }
+            else if (e.KeyCode == Keys.Left && WIN())
+            {
+                e.Handled = true;
+                //Do what you want when this key combination is pressed
+            }
+            */
+
+            if (e.KeyCode == Keys.R && WIN())
+            {
+                e.Handled = true;
+                //App.Current.Dispatcher.BeginInvoke(new Action(() =>
+                App.Current.MainWindow.WindowState = System.Windows.WindowState.Normal;
+                App.Current.MainWindow.Show();
+                App.Current.MainWindow.Activate();
+
+            }
+
+        }
+
+        private static void HookManager_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Used for overriding the Windows default hotkeys
+            while (keysDown.Contains(e.KeyCode))
+            {
+                keysDown.Remove(e.KeyCode);
+            }
+        }
+
+        private static void HookManager_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Used for overriding the Windows default hotkeys
+
+        }
+
+        public static bool CTRL()
+        {
+            //return keysDown.Contains(Keys.LShiftKey)
+            if (keysDown.Contains(Keys.LControlKey) ||
+                keysDown.Contains(Keys.RControlKey) ||
+                keysDown.Contains(Keys.Control) ||
+                keysDown.Contains(Keys.ControlKey))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool SHIFT()
+        {
+            //return keysDown.Contains(Keys.LShiftKey)
+            if (keysDown.Contains(Keys.LShiftKey) ||
+                keysDown.Contains(Keys.RShiftKey) ||
+                keysDown.Contains(Keys.Shift) ||
+                keysDown.Contains(Keys.ShiftKey))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool WIN()
+        {
+            //return keysDown.Contains(Keys.LShiftKey)
+            if (keysDown.Contains(Keys.LWin) ||
+                keysDown.Contains(Keys.RWin))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public static bool ALT()
+        {
+            //return keysDown.Contains(Keys.LShiftKey)
+            if (keysDown.Contains(Keys.Alt) ||
+                keysDown.Contains(Keys.LMenu) ||
+                keysDown.Contains(Keys.RMenu))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
